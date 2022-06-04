897c99a0eb075c0b2e1e97dd48fa4d3f74bd4f46
Ozan Yücel <68647366+ozanyucell@users.noreply.github.com>
2022-06-04 19:00:47 +0300

    nabim but v2

--- sap.py
+++ sap.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import hashlib
+import random
 
 def sha1(id):
     hashed = hashlib.sha1(bytes(f"{id}",encoding="utf-8")).hexdigest()
     return hashed
 
-def concatenate(id, hashed):
-    return f"{id};{hashed}"
+def concatenate(x, y):
+    return f"{x};{y}"
 
+def session_key_generator():
+    return int(random.uniform(1000000000, 9999999999))
+
+'''
 def key_generator():
     # select prime numbers
     p=17
@@ -25,15 +30,16 @@ def key_generator():
         
     d = pow(e, -1, phi)
 
-    public_key = f"{e}, {n}"
-    private_key = f"{d}, {n}"
+    public_key = f"{e};{n}"
+    private_key = f"{d};{n}"
     
-    return "Private: {" + private_key + "}\nPublic: {" + public_key  + "}"
+    return public_key, private_key
 
 def GCD(x, y):
     while y != 0:
         x, y = y, x % y
     return x
+'''
 
 def main():
     with open("./secure_authentication_protocol/ID.txt", mode="r", encoding="UTF-8") as file:

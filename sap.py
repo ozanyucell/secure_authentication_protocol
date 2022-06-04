1b7c0a9bfc506fe9987892bd2d99345069ec07b8
Ozan Yücel <68647366+ozanyucell@users.noreply.github.com>
2022-06-04 13:56:24 +0300

    hotfix

--- sap.py
+++ sap.py
@@ -3,17 +3,14 @@
 import hashlib
 
 def sha1(id):
-    hashed = hashlib.sha1(bytes(f"{id}",encoding="utf-8"))
-    pbHash = hashed.hexdigest()
+    hashed = hashlib.sha1(bytes(f"{id}",encoding="utf-8")).hexdigest()
 
-    print(hashed)
-    print(pbHash)
-    print("ffa16cb29ac811eeaa2e6f70923b1a4fccad55aa")
+    return hashed
 
 def main():
     with open("./secure_authentication_protocol/ID.txt", mode="r", encoding="UTF-8") as file:
         id = file.read()
-    sha1(id)
+    hashed = sha1(id)
 
 
 if __name__ == "__main__":

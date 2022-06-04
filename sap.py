698121b34213c4da1d6934f5ae9e3015dff1b0aa
Ozan Yücel <68647366+ozanyucell@users.noreply.github.com>
2022-06-04 22:27:09 +0300

    AAAAAAAAAAAAAAAA

--- sap.py
+++ sap.py
@@ -7,27 +7,25 @@ def sha1(id):
     hashed = hashlib.sha1(bytes(f"{id}",encoding="utf-8")).hexdigest()
     return hashed
 
-def concatenate(x, y):
+def conc(x, y):
     return f"{x};{y}"
 
 def session_key_generator():
     return int(random.uniform(1000000000, 9999999999))
 
-'''
 def key_generator():
     # select prime numbers
-    p=17
-    q=11
-    e=7
+    p=25423
+    q=29633
+    e=2
     n = p * q
-
     phi = (p-1) * (q-1)
 
-    while(e<phi):
+    while (e<phi):
         if (GCD(e,phi)==1):
             break
         e+=1
-        
+
     d = pow(e, -1, phi)
 
     public_key = f"{e};{n}"
@@ -39,12 +37,24 @@ def GCD(x, y):
     while y != 0:
         x, y = y, x % y
     return x
-'''
+
+def RSA(key, hashed_id):
+    return (int(hashed_id, 16) ** int(key.split(";")[0])) % int(key.split(";")[1])
 
 def main():
     with open("./secure_authentication_protocol/ID.txt", mode="r", encoding="UTF-8") as file:
         id = file.read()
+
     hashed = sha1(id)
+    public_key, private_key = key_generator()
+
+    k_h = str(int(public_key.split(";")[0]) ^ int(private_key.split(";")[0])) + ";" + private_key.split(";")[1]
+
+    cipher_id = hex(RSA(k_h, hashed))[2:]
+
+    print(hashed)
+    print(str(public_key) + " ^ " + str(private_key) + " = " + str(k_h))
+    print(cipher_id)
 
 if __name__ == "__main__":
     main()
---
+++ test.py
@@ -0,0 +1,7 @@
+test = int("ffa16cb29ac811eeaa2e6f70923b1a4fccad55aa", 16)
+
+testSTR = hex(test)[2:]
+
+print(test)
+print(testSTR)
+print("ffa16cb29ac811eeaa2e6f70923b1a4fccad55aa")
